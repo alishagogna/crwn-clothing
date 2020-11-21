d29230eeedc9c326884280d80b2d1fd578e11cca
GOGNA Alisha <agogna@systra.info>
2020-11-21 17:38:49 +0530

    changed the shop data array to object and removed find method

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -1,8 +1,30 @@
 import { combineReducers} from 'redux';
+import { persistReducer } from  'redux-persist';
+import storage from 'redux-persist/lib/storage'; // use localstorage as default storage, alernatively we can also make use of session storage
 import userReducer from './user/user.reducer';
 import cartReducer from './cart/cart.reducer';
+import directoryReducer from './directory/directory.reducer';
+import shopReducer from './shop/shop.reducer';
 
-export default combineReducers({
-    user: userReducer,
-    cart: cartReducer
+const persistConfig = {
+    key:'root', //?
+    storage,// what storage we are using
+    whitelist: ['cart'] //what all reducer you want to store in localstorage
+}
+
+//code b4 using redux -persist
+// export default combineReducers({
+//     user: userReducer, //managed by firebase, no need to store in localstorage(persist it)
+//     cart: cartReducer // persist this in localstorage
+// })
+
+
+//code aftr using redux -persist
+const rootReducer = combineReducers({
+    user: userReducer, //managed by firebase, no need to store in localstorage(persist it)
+    cart: cartReducer, // persist this in localstorage
+    directory:directoryReducer,
+    shop:shopReducer,
 })
+
+export default persistReducer(persistConfig, rootReducer)
